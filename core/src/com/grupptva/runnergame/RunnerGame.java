9c8a1397dfa6855b03044ba9fe19b039b97ed9fc
Aoldo <smurfpojken@gmail.com>
2017-04-02 17:36:32 +0200

    Create branch of hacked together prototype so others can look at it

--- core/src/com/grupptva/runnergame/RunnerGame.java
+++ core/src/com/grupptva/runnergame/RunnerGame.java
@@ -5,23 +5,29 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.grupptva.runnergame.character.CharacterController;
 
 public class RunnerGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
+	CharacterController cc = new CharacterController();
+	
+	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		//img = new Texture("badlogic.jpg");
-	}
+		batch = new SpriteBatch();	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0.3f, 0.6f, 1f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		batch.begin();
-		//batch.draw(img, 0, 0);
+		
+		cc.update();
+		cc.render(batch);
+		
 		batch.end();
 	}
 	
