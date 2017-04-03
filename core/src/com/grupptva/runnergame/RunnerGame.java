4e8a2dc029a2ffe37398d54bf5c6e812acb96e01
Aoldo <smurfpojken@gmail.com>
2017-04-03 13:34:54 +0200

    Add timestep, causes the games logic toonly be run if if enough time has passed since the last frame, also makes it run multiple times if too much time has passed.

--- core/src/com/grupptva/runnergame/RunnerGame.java
+++ core/src/com/grupptva/runnergame/RunnerGame.java
@@ -2,6 +2,7 @@ package com.grupptva.runnergame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -13,6 +14,12 @@ public class RunnerGame extends ApplicationAdapter {
 
 	GameCore GC = new GameCore();
 
+	final double lowestTimePerLogicFrame = 0.0041666; //240fps
+	final double highestTimePerLogicFrame = 0.0166666; //60fps
+
+	double currentTimePerLogicFrame = highestTimePerLogicFrame;
+	double timeAccumulator = 0;
+
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
@@ -20,15 +27,38 @@ public class RunnerGame extends ApplicationAdapter {
 
 	@Override
 	public void render() {
-
 		Gdx.graphics.setTitle(String.valueOf(Gdx.graphics.getFramesPerSecond()));
+
+		//Temporary code to change execution speed.
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			if(currentTimePerLogicFrame > lowestTimePerLogicFrame)
+			{
+				currentTimePerLogicFrame -= 0.0001f;
+			}
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			if(currentTimePerLogicFrame < highestTimePerLogicFrame)
+			{
+				currentTimePerLogicFrame += 0.0001f;
+			}
+		}
+
+		
+		timeAccumulator += Gdx.graphics.getDeltaTime();		
+		
+		while (timeAccumulator > 0) {	
+			timeAccumulator -= currentTimePerLogicFrame;
+			
+			//TODO: Do logic in here.
+			GC.update();
+		}
 
 		Gdx.gl.glClearColor(0.3f, 0.6f, 1f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		batch.begin();
 
-		GC.update();
+		//TODO: Do rendering here.
 		GC.render(batch);
 
 		batch.end();
