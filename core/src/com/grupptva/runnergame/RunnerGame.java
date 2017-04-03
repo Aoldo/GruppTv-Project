8fb855b9b7d481a01662340d5d07088dcf88fcd0
Aoldo <smurfpojken@gmail.com>
2017-04-03 13:41:04 +0200

    Add comment on how timestep works.

--- core/src/com/grupptva/runnergame/RunnerGame.java
+++ core/src/com/grupptva/runnergame/RunnerGame.java
@@ -31,24 +31,35 @@ public class RunnerGame extends ApplicationAdapter {
 
 		//Temporary code to change execution speed.
 		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			if(currentTimePerLogicFrame > lowestTimePerLogicFrame)
-			{
+			if (currentTimePerLogicFrame > lowestTimePerLogicFrame) {
 				currentTimePerLogicFrame -= 0.0001f;
 			}
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			if(currentTimePerLogicFrame < highestTimePerLogicFrame)
-			{
+			if (currentTimePerLogicFrame < highestTimePerLogicFrame) {
 				currentTimePerLogicFrame += 0.0001f;
 			}
 		}
 
-		
-		timeAccumulator += Gdx.graphics.getDeltaTime();		
-		
-		while (timeAccumulator > 0) {	
+		/*
+		 * This part accumulates the amount of time that has passed between this
+		 * and the last frame and then loops through running the game simulation
+		 * until the accumulated time has been expended. This causes the game to
+		 * always run at the same rate, even if the computer is too slow
+		 * (assuming it is too slow because of the graphics! If the simulation
+		 * is the cause this loop might lead to the entire game slowing down and
+		 * then stopping completely!), alternatively if the computer is too
+		 * quick it will prevent the simulation from running too fast.
+		 * 
+		 * Additionally by changing {@See currentTimePerLogicFrame} it is
+		 * possible to change the speed of the simulation. Game is designed to
+		 * be run at any speed between {@See lowestTimePerLogicFrame} and {@See
+		 * highestTimePerLogicFrame}.
+		 */
+		timeAccumulator += Gdx.graphics.getDeltaTime();
+		while (timeAccumulator > 0) {
 			timeAccumulator -= currentTimePerLogicFrame;
-			
+
 			//TODO: Do logic in here.
 			GC.update();
 		}
