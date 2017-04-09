ff6031ca1b3ce6e886f9ae720a32d5b8745590e1
Aoldo <smurfpojken@gmail.com>
2017-04-09 20:46:47 +0200

    Add variable declarations, constructor and empty methods.

--- core/src/com/grupptva/runnergame/worldgenerator/WorldGenerator.java
+++ core/src/com/grupptva/runnergame/worldgenerator/WorldGenerator.java
@@ -1,5 +1,54 @@
 package com.grupptva.runnergame.worldgenerator;
 
-public class WorldGenerator {
+import java.util.List;
 
+public class WorldGenerator {
+	/**
+	 * The offsets compared to the "current tile" that the character can attach
+	 * it's hook to.
+	 */
+	List<Integer[]> hookAttachOffsets;
+	/**
+	 * The offsets compared to the "current tile" that the character can jump
+	 * and land on.
+	 */
+	List<Integer[]> jumpOffsets;
+	/**
+	 * The offsets compared to the "current tile"-which the hook is attached to
+	 * that the character can land on.
+	 * 
+	 * TODO: This might depend on the radius of the hook and will probably cause
+	 * issues later, need to figure out a fix.
+	 */
+	List<Integer[]> hookJumpOffsets;
+	Long seed;
+
+	/**
+	 * Creates a new WorldGenerator, parameters are the coordinates in the grid
+	 * that the character can reach in different ways, in relation the the tile
+	 * the character is currently standing on, or attached to. The offset should
+	 * be in integers that represent the index of the tiles in question inside
+	 * of a chunk.
+	 * 
+	 * @param hookAttachOffsets
+	 * @param jumpOffsets
+	 * @param hookJumpOffsets
+	 * @param seed
+	 */
+	public WorldGenerator(List<Integer[]> hookAttachOffsets, List<Integer[]> jumpOffsets,
+			List<Integer[]> hookJumpOffsets, Long seed) {
+		this.hookAttachOffsets = hookAttachOffsets;
+		this.jumpOffsets = jumpOffsets;
+		this.hookJumpOffsets = hookJumpOffsets;
+		this.seed = seed;
+	}
+
+	public Integer[][] generateChunk(int y) {
+		
+		return null;
+	}
+
+	public List<Integer[][]> generateChunkLog(int y) {
+		return null;
+	}
 }
