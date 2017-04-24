1ab58f4f2beeb6d2c098714e9d4c2cd0120aa488
karl <karlwik@student.chalmers.se>
2017-04-24 19:15:47 +0200

    Add getter and setter for height variable

--- core/src/com/grupptva/runnergame/world/Chunk.java
+++ core/src/com/grupptva/runnergame/world/Chunk.java
@@ -35,4 +35,12 @@ public class Chunk {
 	public void setWidth(int width) {
 		this.width = width;
 	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+	}
 }
