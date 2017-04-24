1e1f4c8bd799790f61e07a827905a7d4e738bed1
karl <karlwik@student.chalmers.se>
2017-04-24 19:06:25 +0200

    Create Chunk.java

---
+++ core/src/com/grupptva/runnergame/world/Chunk.java
@@ -0,0 +1,15 @@
+package com.grupptva.runnergame.world;
+
+/**
+ * Created on 4/24/17.
+ *
+ * The Chunk class represents a piece of the world. It's built up out of {@link Tile}s
+ * and should be the buffer for the {@link WorldModel}.
+ *
+ * @author Karl 'NaN' Wikström
+ */
+public class Chunk {
+	
+
+	
+}
