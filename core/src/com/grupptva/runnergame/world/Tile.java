4b6ab16b329de06fd596e0c75797f4a5cba42c82
karl <karlwik@student.chalmers.se>
2017-04-24 18:52:39 +0200

    Create Tile.java

---
+++ core/src/com/grupptva/runnergame/world/Tile.java
@@ -0,0 +1,15 @@
+package com.grupptva.runnergame.world;
+
+/**
+ * Created on 4/24/17.
+ *
+ * The Tile class represents the data of one element in the grid specified in
+ * {@link WorldModel}.
+ *
+ * @author Karl 'NaN' Wikström
+ */
+public class Tile {
+	
+
+	
+}
