8f0a2dc2188d7e09ecf2cef18c25705b4da5e347
agnesmardh <agnes.mardh@gmail.com>
2017-04-25 16:42:59 +0200

    Change field access to go via getter for position

--- core/test/com/grupptva/runnergame/gamecharacter/GameCharacterTest.java
+++ core/test/com/grupptva/runnergame/gamecharacter/GameCharacterTest.java
@@ -19,25 +19,25 @@ public class GameCharacterTest {
 	public void testMoveY() { 
 		double newY = -1; 
 		gameCharacter.moveY(newY); 
-		Assert.assertTrue(gameCharacter.position.getY() == newY); 
-		Assert.assertTrue(gameCharacter.position.getX() == 0); 
+		Assert.assertTrue(gameCharacter.getPosition().getY() == newY); 
+		Assert.assertTrue(gameCharacter.getPosition().getX() == 0); 
 	}
 
 	@Test
 	public void testFall() { 
 		double yVelocity = gameCharacter.getyVelocity();
-		 double y = gameCharacter.position.getY(); 
+		 double y = gameCharacter.getPosition().getY(); 
 		gameCharacter.fall(); 
 		Assert.assertTrue(gameCharacter.getyVelocity() == yVelocity + gameCharacter.getGravity()); 
-		Assert.assertTrue(gameCharacter.position.getY() == y + yVelocity); 
+		Assert.assertTrue(gameCharacter.getPosition().getY() == y + yVelocity); 
 	}
 
 	@Test
 	public void testJump() { 
-		double x = gameCharacter.position.getX(); 
+		double x = gameCharacter.getPosition().getX(); 
 		double yvelocity = gameCharacter.getyVelocity(); 
 		double jumpVelocity = gameCharacter.getJumpInitialVelocity(); gameCharacter.jump(); 
-		double newX = gameCharacter.position.getX(); 
+		double newX = gameCharacter.getPosition().getX(); 
 		Assert.assertTrue(gameCharacter.getyVelocity() == yvelocity + jumpVelocity); 
 		Assert.assertTrue(x == newX); 
 	} 
