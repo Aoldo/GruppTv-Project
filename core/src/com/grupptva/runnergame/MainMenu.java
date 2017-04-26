fd06e13fb359cd750204d4c5fc1302d7e47a9fce
Luka Mrkonjic <lukamrkonjic163@gmail.com>
2017-04-26 20:56:43 +0200

    Structured the main menu a bit

--- core/src/com/grupptva/runnergame/MainMenu.java
+++ core/src/com/grupptva/runnergame/MainMenu.java
@@ -3,15 +3,39 @@ package com.grupptva.runnergame;
 import java.util.ArrayList;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class MainMenu {
+	
+	int screenWidth = Gdx.graphics.getWidth();
+	int screenHeight = Gdx.graphics.getHeight();
+	
+	startGameString = "Start Game";
+	startGameBF = new BitmapFont();
+	
+	MenuButton startGame;
+	MenuButton highScores;
+	MenuButton quitGame;
+	
+	Texture img;
+	
+	img = new Texture("mainmenubg.png");
+	
+	BitmapFont startGameBF;
+	private String startGameString;
 
     private static ArrayList<MenuButton> menuButtons = new ArrayList<MenuButton>();
 	
 	public MainMenu() {
 		menuButtons.add(new MenuButton(0, 0, 0, 0));
+		
+		startGame = new MenuButton(screenWidth/2-80,screenHeight/2+40,160,40);
+		highScores = new MenuButton(screenWidth/2-80,screenHeight/2-20,160,40);
+		quitGame = new MenuButton(screenWidth/2-80,screenHeight/2-80,160,40);
 	}
 	
     public static void renderAllMenuButtons(ShapeRenderer sr){
@@ -20,7 +44,15 @@ public class MainMenu {
         });
     }
     
-	public void render (ShapeRenderer sr) {
+	public void renderBackground (ShapeRenderer sr, Batch b) {
+		
+	}
+	
+	public void renderButtons (ShapeRenderer sr, Batch b) {
+		
+	}
+	
+	public void renderText (ShapeRenderer sr, Batch b) {
 		
 	}
 
--- core/src/com/grupptva/runnergame/RunnerGame.java
+++ core/src/com/grupptva/runnergame/RunnerGame.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
@@ -12,41 +13,31 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 public class RunnerGame extends ApplicationAdapter {
 	ShapeRenderer sr;
 	SpriteBatch batch;
-	Texture img;
-	MenuButton startGame;
-	MenuButton highScores;
-	MenuButton quitGame;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		sr = new ShapeRenderer();
-		
-		int screenWidth = Gdx.graphics.getWidth();
-		int screenHeight = Gdx.graphics.getHeight();
-		
-		//mb = new MenuButton(Gdx.graphics.getWidth()/2,Gdx.graphics.getHeight()/2,20,20);
-		//mb = new MenuButton((Gdx.graphics.getWidth() - width/2),(Gdx.graphics.getHeight() - height/2),20,20);
-		startGame = new MenuButton(screenWidth/2-80,screenHeight/2+40,160,40);
-		highScores = new MenuButton(screenWidth/2-80,screenHeight/2-20,160,40);
-		quitGame = new MenuButton(screenWidth/2-80,screenHeight/2-80,160,40);
-		img = new Texture("mainmenubg.png");
-		//this.setScreen(new StartScreen(this));
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0.3f, 0.6f, 1f, 0.4f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.end();
+		
 		sr.begin(ShapeType.Filled);
 		//sr.begin(ShapeType.Line);
 		startGame.render(sr);
 		highScores.render(sr);
 		quitGame.render(sr);
 		sr.end();
+		batch.begin();
+		startGameBF.draw(batch, startGameString, Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2); 
+		batch.end();
 	}
 	
 	@Override
