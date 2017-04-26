e28b272f86aa284c75bab12bace14f84dc792049
agnesmardh <agnes.mardh@gmail.com>
2017-04-26 08:32:13 +0200

    Remove \u2028 character

--- core/test/com/grupptva/runnergame/gamecharacter/GameCharacterTest.java
+++ core/test/com/grupptva/runnergame/gamecharacter/GameCharacterTest.java
@@ -11,34 +11,34 @@ public class GameCharacterTest {
 	private GameCharacter gameCharacter;
 
 	@Before
-	public void setUp() { 
-		gameCharacter = new GameCharacter(0, 0); 
+	public void setUp() {
+		gameCharacter = new GameCharacter(0, 0);
 	}
 
 	@Test
-	public void testMoveY() { 
-		double newY = -1; 
-		gameCharacter.moveY(newY); 
-		Assert.assertTrue(gameCharacter.getPosition().getY() == newY); 
-		Assert.assertTrue(gameCharacter.getPosition().getX() == 0); 
+	public void testMoveY() {
+		double newY = -1;
+		gameCharacter.moveY(newY);
+		Assert.assertTrue(gameCharacter.getPosition().getY() == newY);
+		Assert.assertTrue(gameCharacter.getPosition().getX() == 0);
 	}
 
 	@Test
-	public void testFall() { 
+	public void testFall() {
 		double yVelocity = gameCharacter.getyVelocity();
-		 double y = gameCharacter.getPosition().getY(); 
-		gameCharacter.fall(); 
-		Assert.assertTrue(gameCharacter.getyVelocity() == yVelocity + gameCharacter.getGravity()); 
-		Assert.assertTrue(gameCharacter.getPosition().getY() == y + yVelocity); 
+		double y = gameCharacter.getPosition().getY();
+		gameCharacter.fall();
+		Assert.assertTrue(gameCharacter.getyVelocity() == yVelocity + gameCharacter.getGravity());
+		Assert.assertTrue(gameCharacter.getPosition().getY() == y + yVelocity);
 	}
 
 	@Test
-	public void testJump() { 
-		double x = gameCharacter.getPosition().getX(); 
-		double yvelocity = gameCharacter.getyVelocity(); 
-		double jumpVelocity = gameCharacter.getJumpInitialVelocity(); gameCharacter.jump(); 
-		double newX = gameCharacter.getPosition().getX(); 
-		Assert.assertTrue(gameCharacter.getyVelocity() == yvelocity + jumpVelocity); 
-		Assert.assertTrue(x == newX); 
-	} 
+	public void testJump() {
+		double x = gameCharacter.getPosition().getX();
+		double yvelocity = gameCharacter.getyVelocity();
+		double jumpVelocity = gameCharacter.getJumpInitialVelocity();gameCharacter.jump();
+		double newX = gameCharacter.getPosition().getX();
+		Assert.assertTrue(gameCharacter.getyVelocity() == yvelocity + jumpVelocity);
+		Assert.assertTrue(x == newX);
+	}
 }
