5113a93a484fff6f0990196e0ee012263a44d9c3
Aoldo <smurfpojken@gmail.com>
2017-05-09 15:01:37 +0200

    Add testing method for calculateJumpLandingOffsets. Add testing methods for sorting methods.

--- core/test/com/grupptva/runnergame/game/services/WorldGeneratorTest.java
+++ core/test/com/grupptva/runnergame/game/services/WorldGeneratorTest.java
@@ -3,6 +3,7 @@ package com.grupptva.runnergame.game.services;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.*;
@@ -20,8 +21,8 @@ public class WorldGeneratorTest {
 
 	@Before
 	public void init() {
-		wg = new WorldGenerator(new ArrayList<Integer[]>(), new ArrayList<Integer[]>(), new ArrayList<Integer[]>(), 2l,
-				40, 20, 0);
+		wg = new WorldGenerator(new ArrayList<Integer[]>(), new ArrayList<Integer[]>(),
+				new ArrayList<Integer[]>(), 2l, 40, 20, 0);
 	}
 
 	@Test
@@ -36,8 +37,8 @@ public class WorldGeneratorTest {
 
 	@Test
 	public void getFramesToYValueTest() {
-		assertTrue(wg.getFramesToYValue(10, -10, 0, 0) == wg.getFramesToApexOfJump(10, 10) * 2);
-		System.out.println(wg.getFramesToYValue(4, -2, -5, 1));
+		assertTrue(wg.getFramesToYValue(10, -10, 0, 0) == wg.getFramesToApexOfJump(10, 10)
+				* 2);
 		assertEquals(wg.getFramesToYValue(4, -2, -5, 1), 5.15f, .1f);
 	}
 
@@ -47,17 +48,114 @@ public class WorldGeneratorTest {
 	}
 
 	@Test
+	public void mergeSortTest() {
+		List<Integer[]> expectedList = new ArrayList<Integer[]>();
+		expectedList.add(new Integer[] { 0, 1 });
+		expectedList.add(new Integer[] { 0, 2 });
+		expectedList.add(new Integer[] { 1, 3 });
+		expectedList.add(new Integer[] { 1, 5 });
+		expectedList.add(new Integer[] { 2, 1 });
+		expectedList.add(new Integer[] { 2, 2 });
+		expectedList.add(new Integer[] { 3, 3 });
+		expectedList.add(new Integer[] { 3, 5 });
+		expectedList.add(new Integer[] { 4, 1 });
+		expectedList.add(new Integer[] { 4, 2 });
+
+		List<Integer[]> sortList = new ArrayList<Integer[]>();
+		sortList.add(new Integer[] { 4, 1 });
+		sortList.add(new Integer[] { 0, 2 });
+		sortList.add(new Integer[] { 1, 5 });
+		sortList.add(new Integer[] { 2, 1 });
+		sortList.add(new Integer[] { 1, 3 });
+		sortList.add(new Integer[] { 3, 5 });
+		sortList.add(new Integer[] { 3, 3 });
+		sortList.add(new Integer[] { 4, 2 });
+		sortList.add(new Integer[] { 2, 2 });
+		sortList.add(new Integer[] { 0, 1 });
+
+		//Sort by Y first and then X to get it sorted primarily by X and secondarily by Y
+		//Works because of stable sorting method.
+		sortList = wg.mergeSort(sortList, 1);
+		sortList = wg.mergeSort(sortList, 0);
+
+		for (int i = 0; i < sortList.size(); i++) {
+			for (int u = 0; u < sortList.get(i).length; u++) {
+				assertTrue(sortList.get(i)[u] == expectedList.get(i)[u]);
+			}
+		}
+	}
+
+	@Test
+	public void sortJumpIndexesTest() {
+		List<Integer[]> expectedList = new ArrayList<Integer[]>();
+		expectedList.add(new Integer[] { 0, 1 });
+		expectedList.add(new Integer[] { 0, 2 });
+		expectedList.add(new Integer[] { 1, 3 });
+		expectedList.add(new Integer[] { 1, 5 });
+		expectedList.add(new Integer[] { 2, 2 });
+		expectedList.add(new Integer[] { 2, 1 });
+		expectedList.add(new Integer[] { 3, 5 });
+		expectedList.add(new Integer[] { 3, 3 });
+		expectedList.add(new Integer[] { 4, 2 });
+		expectedList.add(new Integer[] { 4, 1 });
+
+		List<Integer[]> sortList = new ArrayList<Integer[]>();
+		sortList.add(new Integer[] { 4, 1 });
+		sortList.add(new Integer[] { 0, 2 });
+		sortList.add(new Integer[] { 1, 5 });
+		sortList.add(new Integer[] { 2, 1 });
+		sortList.add(new Integer[] { 1, 3 });
+		sortList.add(new Integer[] { 3, 5 });
+		sortList.add(new Integer[] { 3, 3 });
+		sortList.add(new Integer[] { 4, 2 });
+		sortList.add(new Integer[] { 2, 2 });
+		sortList.add(new Integer[] { 0, 1 });
+
+		sortList = wg.sortJumpIndexes(sortList);
+
+		for (int i = 0; i < sortList.size(); i++) {
+			for (int u = 0; u < sortList.get(i).length; u++) {
+				assertTrue(sortList.get(i)[u] == expectedList.get(i)[u]);
+			}
+		}
+	}
+
+	@Test
+	public void calculateJumpLandingOffsetsTest() {
+		List<Integer[]> landingOffsets = wg.calculateJumpLandingOffsets(4, -2, 1, 1);
+		List<Integer[]> expectedOffsets = new ArrayList<Integer[]>();
+
+		expectedOffsets.add(new Integer[] { 3, 6 });
+		expectedOffsets.add(new Integer[] { 3, 5 });
+		expectedOffsets.add(new Integer[] { 3, 4 });
+		expectedOffsets.add(new Integer[] { 4, 3 });
+		expectedOffsets.add(new Integer[] { 4, 2 });
+		expectedOffsets.add(new Integer[] { 4, 1 });
+		expectedOffsets.add(new Integer[] { 4, 0 });
+
+		
+		for (int i = 0; i < landingOffsets.size(); i++) {
+			for (int u = 0; u < landingOffsets.get(i).length; u++) {
+				assertTrue(landingOffsets.get(i)[u] == expectedOffsets.get(i)[u]);
+			}
+		}
+
+	}
+
+	@Test
 	public void calculateJumpGridTest() {
 		//Don't worry about it.
 		boolean[][] result1 = new boolean[][] { { false, false, false, false, true },
-				{ false, false, false, false, true }, { false, false, false, false, true },
+				{ false, false, false, false, true },
+				{ false, false, false, false, true },
 				{ false, false, false, false, true }, { true, false, false, true, true },
 				{ true, false, false, true, false }, { true, true, false, true, false },
 				{ true, true, true, true, false } };
 
 		assertArrayEquals(wg.calculateJumpGrid(4, -2, 1, 1), result1);
 
-		boolean[][] result2 = new boolean[][] { { false, false, false, false, false, false, false, true },
+		boolean[][] result2 = new boolean[][] {
+				{ false, false, false, false, false, false, false, true },
 				{ false, false, false, false, false, false, false, true },
 				{ false, false, false, false, false, false, true, true },
 				{ false, false, false, false, false, false, true, false },
@@ -80,7 +178,8 @@ public class WorldGeneratorTest {
 
 	@Test
 	public void deepCopyChunk_ShouldNotBeReference() {
-		Tile[][] chunk = new Tile[][] { { Tile.EMPTY, Tile.EMPTY }, { Tile.EMPTY, Tile.EMPTY } };
+		Tile[][] chunk = new Tile[][] { { Tile.EMPTY, Tile.EMPTY },
+				{ Tile.EMPTY, Tile.EMPTY } };
 
 		Tile[][] chunkCopy = wg.deepCopyChunk(chunk);
 		chunk[0][0] = Tile.FULL;
