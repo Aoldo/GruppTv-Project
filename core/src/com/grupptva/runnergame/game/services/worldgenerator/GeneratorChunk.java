3c4f60ee87d622ddb914ce73365301ec35faa7be
Aoldo <smurfpojken@gmail.com>
2017-05-19 15:06:46 +0200

    Fix import error caused by deleting obsolete files.

--- core/src/com/grupptva/runnergame/game/services/worldgenerator/GeneratorChunk.java
+++ core/src/com/grupptva/runnergame/game/services/worldgenerator/GeneratorChunk.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import com.grupptva.runnergame.game.services.worldgenerator.WorldGeneratorOLD.Tile;
 
 class GeneratorChunk {
 	public Tile[][] tiles;
