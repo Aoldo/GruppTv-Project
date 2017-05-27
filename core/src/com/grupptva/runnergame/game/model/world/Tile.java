80c1ad04d8e4e15673b963d3b8021e2739faf989
MrLinker <karlwik@student.chalmers.se>
2017-05-27 15:04:41 +0200

    Add javadoc for class

--- core/src/com/grupptva/runnergame/game/model/world/Tile.java
+++ core/src/com/grupptva/runnergame/game/model/world/Tile.java
@@ -3,8 +3,17 @@ package com.grupptva.runnergame.game.model.world;
 /**
  * Created on 4/24/17.
  *
- * The Tile enum represents the data of one element in the grid specified in
- * {@link WorldModel}.
+ * Responsibility: The Tile enum represents the data of one element in the grid
+ * specified in {@link WorldModel}.
+ *
+ * Used by:
+ * @see WorldModel
+ * @see Chunk
+ * @see com.grupptva.runnergame.game.model.CollisionLogic
+ * @see com.grupptva.runnergame.game.model.GameLogic
+ * @see com.grupptva.runnergame.game.model.HookLogic
+ * @see com.grupptva.runnergame.game.model.worldgenerator.WorldGenerator
+ *
  *
  * @author Karl 'NaN' Wikström
  */
